b52badfcd09fdf3ec902a9f08c889e50c7833994
dingxiaohan <dingxiaohan@megvii.com>
2019-11-18 17:54:17 +0800

    Fix bug of negative padding

--- acnet/acnet_builder.py
+++ acnet/acnet_builder.py
@@ -1,6 +1,19 @@
 from builder import ConvBuilder
 import torch.nn as nn
 
+class CropLayer(nn.Module):
+
+    #   E.g., (-1, 0) means this layer should crop the first and last rows of the feature map. And (0, -1) crops the first and last columns
+    def __init__(self, crop_set):
+        super(CropLayer, self).__init__()
+        self.rows_to_crop = - crop_set[0]
+        self.cols_to_crop = - crop_set[1]
+        assert self.rows_to_crop >= 0
+        assert self.cols_to_crop >= 0
+
+    def forward(self, input):
+        return input[:, :, self.rows_to_crop:-self.rows_to_crop, self.cols_to_crop:-self.cols_to_crop]
+
 
 class ACBlock(nn.Module):
 
@@ -17,15 +30,29 @@ class ACBlock(nn.Module):
                                          padding_mode=padding_mode)
             self.square_bn = nn.BatchNorm2d(num_features=out_channels)
 
+            center_offset_from_origin_border = padding - kernel_size // 2
+            ver_pad_or_crop = (center_offset_from_origin_border + 1, center_offset_from_origin_border)
+            hor_pad_or_crop = (center_offset_from_origin_border, center_offset_from_origin_border + 1)
+            if center_offset_from_origin_border >= 0:
+                self.ver_conv_crop_layer = nn.Identity()
+                ver_conv_padding = ver_pad_or_crop
+                self.hor_conv_crop_layer = nn.Identity()
+                hor_conv_padding = hor_pad_or_crop
+            else:
+                self.ver_conv_crop_layer = CropLayer(crop_set=ver_pad_or_crop)
+                ver_conv_padding = (0, 0)
+                self.hor_conv_crop_layer = CropLayer(crop_set=hor_pad_or_crop)
+                hor_conv_padding = (0, 0)
             self.ver_conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(3, 1),
                                       stride=stride,
-                                      padding=(padding - kernel_size // 2 + 1, padding - kernel_size // 2), dilation=dilation, groups=groups, bias=False,
+                                      padding=ver_conv_padding, dilation=dilation, groups=groups, bias=False,
                                       padding_mode=padding_mode)
-            self.ver_bn = nn.BatchNorm2d(num_features=out_channels)
+
             self.hor_conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(1, 3),
                                       stride=stride,
-                                      padding=(padding - kernel_size // 2, padding - kernel_size // 2 + 1), dilation=dilation, groups=groups, bias=False,
+                                      padding=hor_conv_padding, dilation=dilation, groups=groups, bias=False,
                                       padding_mode=padding_mode)
+            self.ver_bn = nn.BatchNorm2d(num_features=out_channels)
             self.hor_bn = nn.BatchNorm2d(num_features=out_channels)
 
 
@@ -38,13 +65,16 @@ class ACBlock(nn.Module):
             square_outputs = self.square_bn(square_outputs)
             # print(square_outputs.size())
             # return square_outputs
-            vertical_outputs = self.ver_conv(input)
+            vertical_outputs = self.ver_conv_crop_layer(input)
+            vertical_outputs = self.ver_conv(vertical_outputs)
             vertical_outputs = self.ver_bn(vertical_outputs)
             # print(vertical_outputs.size())
-            horizontal_outputs = self.hor_conv(input)
+            horizontal_outputs = self.hor_conv_crop_layer(input)
+            horizontal_outputs = self.hor_conv(horizontal_outputs)
             horizontal_outputs = self.hor_bn(horizontal_outputs)
             # print(horizontal_outputs.size())
             return square_outputs + vertical_outputs + horizontal_outputs
+
 
 
 class ACNetBuilder(ConvBuilder):
